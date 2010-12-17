564ce6ea084343bdaddde0f26551019382215c35
cgilmer <cgilmer@beta.(none)>
2010-12-17 10:47:13 -0800

    Added rating and size as available settings for the settings.py file.
    Added rating and size to all methods.
    
    Changed the way in which parameters are added to the url.
    
    Changed all methods to use the simple_tag decorator.
    
    Added doc string to the main method.

--- gravatar/templatetags/gravatar.py
+++ gravatar/templatetags/gravatar.py
@@ -1,14 +1,16 @@
-from django import template
-from django.utils.html import escape
-from django.contrib.auth.models import User
-from django.conf import settings
-from django.utils.hashcompat import md5_constructor
-
 import urllib
 
-GRAVATAR_URL_PREFIX = getattr(settings, "GRAVATAR_URL_PREFIX", "http://www.gravatar.com/")
-GRAVATAR_DEFAULT_IMAGE = getattr(settings, "GRAVATAR_DEFAULT_IMAGE", "")
-GRAVATAR_IMG_CLASS = getattr(settings, "GRAVATAR_IMG_CLASS", None)
+from django import template
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.utils.hashcompat import md5_constructor
+from django.utils.html import escape
+
+GRAVATAR_URL_PREFIX     = getattr(settings, "GRAVATAR_URL_PREFIX", "http://www.gravatar.com/avatar/")
+GRAVATAR_DEFAULT_IMAGE  = getattr(settings, "GRAVATAR_DEFAULT_IMAGE", "")
+GRAVATAR_DEFAULT_RATING = getattr(settings, "GRAVATAR_DEFAULT_RATING", "g")
+GRAVATAR_DEFAULT_SIZE   = getattr(settings, "GRAVATAR_DEFAULT_SIZE", 80)
+GRAVATAR_IMG_CLASS      = getattr(settings, "GRAVATAR_IMG_CLASS", None)
 
 def _imgclass_attr():
     if GRAVATAR_IMG_CLASS:
@@ -23,36 +25,54 @@ def get_user(user):
         try:
             user = User.objects.get(username=user)
         except User.DoesNotExist:
-            # TODO: make better? smarter? strong? maybe give it wheaties?
             raise Exception, "Bad user for gravatar."
     return user
 
-def gravatar_for_email(email, size=80):
-    url = "%savatar/%s/?" % (GRAVATAR_URL_PREFIX, md5_constructor(email).hexdigest())
-    url += urllib.urlencode({"s": str(size), "default": GRAVATAR_DEFAULT_IMAGE})
+@register.simple_tag
+def gravatar_for_email(email, size=None, rating=None):
+	"""
+	Generates a Gravatar URL for the given email address.
+
+	Syntax::
+
+		{% gravatar_for_email <email> [size] [rating] %}
+	
+	Example::
+
+		{% gravatar_for_email foobar@example.com 48 pg %}
+	"""
+	gravatar_id = md5_constructor(email).hexdigest()
+    gravatar_url = GRAVATAR_URL_PREFIX + gravatar_id
+	
+	parameters = [p for p in (
+		('d', GRAVATAR_DEFAULT_IMAGE),
+		('s', size or GRAVATAR_DEFAULT_SIZE),
+		('r', rating or GRAVATAR_DEFAULT_RATING),
+	) if p[1]]
+	if parameters:
+    	gravatar_url += urllib.urlencode(parameters, doseq=True)
+
     return escape(url)
 
-def gravatar_for_user(user, size=80):
+@register.simple_tag
+def gravatar_for_user(user, size=None, rating=None):
     user = get_user(user)
-    return gravatar_for_email(user.email, size)
+    return gravatar_for_email(user.email, size, rating)
 
-def gravatar_img_for_email(email, size=80):
-    url = gravatar_for_email(email, size)
+@register.simple_tag
+def gravatar_img_for_email(email, size=None, rating=None):
+    url = gravatar_for_email(email, size, rating)
     return """<img src="%s"%s height="%s" width="%s"/>""" % (escape(url),
             _imgclass_attr(), size, size)
 
-def gravatar_img_for_user(user, size=80):
-    user = get_user(user)
-    url = gravatar_for_user(user, size)
+@register.simple_tag
+def gravatar_img_for_user(user, size=None, rating=None):
+    url = gravatar_for_user(user, size, rating)
     return """<img src="%s"%s alt="Avatar for %s" height="%s" width="%s"/>""" % \
             (escape(url), _imgclass_attr(), user.username, size, size)
 
-def gravatar(user, size=80):
+@register.simple_tag
+def gravatar(user, size=None, rating=None):
     # backward compatibility
-    return gravatar_img_for_user(user, size)
+    return gravatar_img_for_user(user, size, rating)
 
-register.simple_tag(gravatar)
-register.simple_tag(gravatar_for_user)
-register.simple_tag(gravatar_for_email)
-register.simple_tag(gravatar_img_for_user)
-register.simple_tag(gravatar_img_for_email)
